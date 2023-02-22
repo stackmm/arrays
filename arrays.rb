ba42aacd10f0bdff876b9404079ca55af98c845e
Steven Stackhouse <stackmm@gmail.com>
2023-02-22 14:42:26 -0700

    add four array methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,15 @@
 strings_array = ["joe", "mike", "amy", "sarah"]
+# this method will remove the last element of the strings array
+strings_array.pop
 
 integers_array = [1, 4, 55, 29]
+# this method will push the element 7 to the end of the integers array
+integers_array.push(7)
 
 floats_array = [12.1, 33.07, 9.2, 44.44]
+# this method will remove the first element (12.1) of the floats array
+floats_array.shift
 
 booleans_array = [false, true, false, true]
+# this method will add the boolean element "true" to the front of the booleans array
+booleans_array.unshift(true)
